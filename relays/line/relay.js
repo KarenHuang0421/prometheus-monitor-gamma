604744fac3f8da86f57f72ff3a67415ebfaadd98
KarenHuang0421 <a0988423161@gmail.com>
2025-06-12 16:06:12 +0800

    feat: add garfana and refactor structure

--- relays/line/relay.js
+++ relays/line/relay.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
